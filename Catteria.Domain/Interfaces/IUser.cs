158a806e5c28a30edb780d7075137bf52a8a9d69
Daniel <daniotavio433@gmail.com>
2026-06-18 15:12:21 -0300

    fiz as interfaces do produtos e usuario na domain

--- Catteria.Domain/Interfaces/IUser.cs
+++ Catteria.Domain/Interfaces/IUser.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Catteria.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Catteria.Domain.Interfaces
 {
-    internal class IUser
+    public interface IUser
     {
+        Task AddAsync(User user);
+
+        Task UpdateAsync(User user);
+
+        Task DeleteAsync(int id);
     }
 }
