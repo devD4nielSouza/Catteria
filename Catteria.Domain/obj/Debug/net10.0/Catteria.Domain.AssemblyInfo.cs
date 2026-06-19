a4cfb9346b2c5045a74e47f806adf63fbacb30c8
Matheus <matverialmeida2@gmail.com>
2026-06-19 17:26:36 -0300

    gitignore 2

--- Catteria.Domain/obj/Debug/net10.0/Catteria.Domain.AssemblyInfo.cs
+++ Catteria.Domain/obj/Debug/net10.0/Catteria.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catteria.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f249abe2f8b582e9c26feaca9859db9d1b0e1f48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab6ba36c880cbac219e3f05dff02b0248ecb682")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catteria.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catteria.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
