c459a242f908e8e59c52467eff2bed8434c74634
Daniel <daniotavio433@gmail.com>
2026-06-19 14:21:33 -0300

    Arrumando a order e itemorder dto

--- Catteria.Application/DTOs/OrderDto.cs
+++ Catteria.Application/DTOs/OrderDto.cs
@@ -15,7 +15,6 @@ namespace Catteria.Application.DTOs
 
     public class CreateOrderDto
     {
-        public int Id { get; set; }
         public DateTime Date { get; set; } = DateTime.Now; // Data do pedido, definida automaticamente para a data atual
         public decimal TotalValue { get; set; } // Valor total do pedido
         public string Status { get; set; } = string.Empty; // Status do pedido (ex: "Pendente", "Em andamento", "Concluído")
--- Catteria.Application/DTOs/OrderItemDto.cs
+++ Catteria.Application/DTOs/OrderItemDto.cs
@@ -6,9 +6,28 @@ namespace Catteria.Application.DTOs
 {
     public class OrderItemDto
     {
-        public int Id { get; set; }
-        public int Quantity { get; set; }
-        public decimal Subtotal { get; set; }
+        public int Id { get; set; } // Identificador único do item do pedido (chave primária)
+        public int Quantity { get; set; } // Quantidade do produto no item do pedido
+        public decimal SubTotal { get; private set; } // Subtotal do item do pedido (preço do produto multiplicado pela quantidade)
+        public int IdOrder { get; set; } // Chave estrangeira para o pedido (relacionamento com a entidade Order)
+        public int IdProduct { get; set; } // Chave estrangeira para o produto (relacionamento com a entidade Product
+        public decimal UnitPrice { get; set; }
+    }
+
+    public class CreateOrderItemDto
+    {
+        public int Quantity { get; set; } // Quantidade do produto no item do pedido
+        public decimal SubTotal { get; private set; } // Subtotal do item do pedido (preço do produto multiplicado pela quantidade)
+        public int IdOrder { get; set; } // Chave estrangeira para o pedido (relacionamento com a entidade Order)
+        public int IdProduct { get; set; } // Chave estrangeira para o produto (relacionamento com a entidade Product
+        public decimal UnitPrice { get; set; }
+
+    }
+
+    public class UpdateOrderItemDto
+    {
+        public int Quantity { get; set; } // Quantidade do produto no item do pedido
+        public int IdProduct { get; set; } // Chave estrangeira para o produto (relacionamento com a entidade Product
         public decimal UnitPrice { get; set; }
     }
 }
