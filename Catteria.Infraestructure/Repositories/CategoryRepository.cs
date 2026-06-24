968c18c3fa55944647ad4d53491e16569db82fe3
MTheus0 <matverialmeida2@gmail.com>
2026-06-24 17:52:22 -0300

    comecei o OrderRepository e terminei o CategoryRepository

--- Catteria.Infraestructure/Repositories/CategoryRepository.cs
+++ Catteria.Infraestructure/Repositories/CategoryRepository.cs
@@ -25,6 +25,38 @@ namespace Catteria.Infraestructure.Repositories
                 .ToListAsync();
         }
 
-        public async Task<Category?>
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Categories
+                .Include(c => c.Products)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if(category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Categories.CountAsync();
+        }
     }
 }
--- Catteria.Infraestructure/Repositories/OrderRepository.cs
+++ Catteria.Infraestructure/Repositories/OrderRepository.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Catteria.Domain.Entities;
+using Catteria.Domain.Interfaces;
+using Catteria.Infraestructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catteria.Infraestructure.Repositories
 {
-    internal class OrderRepository
+    public class OrderRepository : IOrderRepository
     {
+        private readonly CatteriaDbContext _context;
+
+        public OrderRepository(CatteriaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Order>> GetAllAsync()
+        {
+            return await _context.Orders
+                .Include(o => o.User)
+                .Include(o => o.OrderItems)
+                .OrderByDescending(o => o.Date)
+                .ToListAsync();
+        }
+
+        public async Task<Order?> GetByIdAsync(int id)
+        {
+            return await _context.Orders
+                .Include(o => o.User)
+                .Include(o => o.OrderItems)
+                .OrderByDescending(o => o.Date)
+                .FirstOrDefaultAsync(o => o.Id == id);
+        }
+
+        public async Task AddAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+        }
+
+
     }
 }
