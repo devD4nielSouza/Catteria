db47b7f50195f8ab76a4a86d7319fa284ff6f816
MTheus0 <matverialmeida2@gmail.com>
2026-06-24 15:12:44 -0300

    continuando infra

--- Catteria.Infraestructure/Repositories/CategoryRepository.cs
+++ Catteria.Infraestructure/Repositories/CategoryRepository.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Catteria.Domain.Entities;
+using Catteria.Domain.Interfaces;
+using Catteria.Infraestructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catteria.Infraestructure.Repositories
 {
-    internal class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private readonly CatteriaDbContext _context;
+
+        public CategoryRepository(CatteriaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories
+                .Include(c => c.Products)
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+        }
+
+        public async Task<Category?>
     }
 }
