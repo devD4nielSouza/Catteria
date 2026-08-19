e8dcb93d18b94ad30ff738b1238ba5c38c789a13
graziella <graziellassilva.8@gmail.com>
2026-08-19 15:09:14 -0300

    Desktop Criado

---
+++ Catteria.Desktop/Helpers/HttpClientHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catteria.Desktop.Helpers
+{
+    internal class HttpClientHelper
+    {
+    }
+}
