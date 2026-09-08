33641288a9f97dc320651efb08fdc51e3849040f
Matheus <matverialmeida2@gmail.com>
2026-09-08 15:16:57 -0300

    Começo do CupomFormDialog

--- Catteria.Desktop/UserControls/CupomUserControl.Designer.cs
+++ Catteria.Desktop/UserControls/CupomUserControl.Designer.cs
@@ -28,10 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            txtPesquisa = new Guna.UI2.WinForms.Guna2TextBox();
+            btnEditar = new Guna.UI2.WinForms.Guna2Button();
+            btnExcluir = new Guna.UI2.WinForms.Guna2Button();
+            btnAtualizar = new Guna.UI2.WinForms.Guna2Button();
+            lblTitulo = new Label();
+            gridPedidos = new DataGridView();
+            colId = new DataGridViewTextBoxColumn();
+            colDate = new DataGridViewTextBoxColumn();
+            colPrice = new DataGridViewTextBoxColumn();
+            colStatus = new DataGridViewTextBoxColumn();
+            colClient = new DataGridViewTextBoxColumn();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)gridPedidos).BeginInit();
+            SuspendLayout();
+            // 
+            // txtPesquisa
+            // 
+            txtPesquisa.BorderRadius = 5;
+            txtPesquisa.CustomizableEdges = customizableEdges1;
+            txtPesquisa.DefaultText = "";
+            txtPesquisa.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtPesquisa.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtPesquisa.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtPesquisa.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtPesquisa.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtPesquisa.Font = new Font("Segoe UI", 9F);
+            txtPesquisa.ForeColor = Color.DimGray;
+            txtPesquisa.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtPesquisa.Location = new Point(64, 75);
+            txtPesquisa.Name = "txtPesquisa";
+            txtPesquisa.PlaceholderForeColor = Color.Gray;
+            txtPesquisa.PlaceholderText = "Pesquisa por nome";
+            txtPesquisa.SelectedText = "";
+            txtPesquisa.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            txtPesquisa.Size = new Size(293, 36);
+            txtPesquisa.TabIndex = 19;
+            // 
+            // btnEditar
+            // 
+            btnEditar.BorderRadius = 5;
+            btnEditar.CustomizableEdges = customizableEdges3;
+            btnEditar.DisabledState.BorderColor = Color.DarkGray;
+            btnEditar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnEditar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnEditar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnEditar.FillColor = Color.FromArgb(64, 162, 66);
+            btnEditar.Font = new Font("Segoe UI", 9F);
+            btnEditar.ForeColor = Color.White;
+            btnEditar.Location = new Point(452, 66);
+            btnEditar.Name = "btnEditar";
+            btnEditar.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            btnEditar.Size = new Size(90, 45);
+            btnEditar.TabIndex = 16;
+            btnEditar.Text = "Editar";
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.BorderRadius = 5;
+            btnExcluir.CustomizableEdges = customizableEdges5;
+            btnExcluir.DisabledState.BorderColor = Color.DarkGray;
+            btnExcluir.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnExcluir.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnExcluir.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnExcluir.FillColor = Color.FromArgb(209, 73, 63);
+            btnExcluir.Font = new Font("Segoe UI", 9F);
+            btnExcluir.ForeColor = Color.White;
+            btnExcluir.Location = new Point(548, 66);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            btnExcluir.Size = new Size(90, 45);
+            btnExcluir.TabIndex = 17;
+            btnExcluir.Text = "Excluir";
+            // 
+            // btnAtualizar
+            // 
+            btnAtualizar.BorderRadius = 5;
+            btnAtualizar.CustomizableEdges = customizableEdges7;
+            btnAtualizar.DisabledState.BorderColor = Color.DarkGray;
+            btnAtualizar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnAtualizar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnAtualizar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnAtualizar.FillColor = Color.FromArgb(219, 209, 89);
+            btnAtualizar.Font = new Font("Segoe UI", 9F);
+            btnAtualizar.ForeColor = Color.White;
+            btnAtualizar.Location = new Point(644, 66);
+            btnAtualizar.Name = "btnAtualizar";
+            btnAtualizar.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            btnAtualizar.Size = new Size(90, 45);
+            btnAtualizar.TabIndex = 18;
+            btnAtualizar.Text = "Atualizar";
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitulo.ForeColor = Color.FromArgb(64, 64, 64);
+            lblTitulo.Location = new Point(58, 25);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(222, 25);
+            lblTitulo.TabIndex = 15;
+            lblTitulo.Text = "Gerenciador de Cupons";
+            // 
+            // gridPedidos
+            // 
+            gridPedidos.BackgroundColor = SystemColors.ActiveCaption;
+            gridPedidos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridPedidos.Columns.AddRange(new DataGridViewColumn[] { colId, colDate, colPrice, colStatus, colClient });
+            gridPedidos.GridColor = SystemColors.InactiveCaption;
+            gridPedidos.Location = new Point(64, 117);
+            gridPedidos.Name = "gridPedidos";
+            gridPedidos.Size = new Size(670, 340);
+            gridPedidos.TabIndex = 14;
+            // 
+            // colId
+            // 
+            colId.HeaderText = "ID";
+            colId.Name = "colId";
+            colId.Width = 80;
+            // 
+            // colDate
+            // 
+            colDate.HeaderText = "DataPedido";
+            colDate.Name = "colDate";
+            colDate.Width = 120;
+            // 
+            // colPrice
+            // 
+            colPrice.HeaderText = "Valor Total";
+            colPrice.Name = "colPrice";
+            // 
+            // colStatus
+            // 
+            colStatus.HeaderText = "Status";
+            colStatus.Name = "colStatus";
+            colStatus.Width = 110;
+            // 
+            // colClient
+            // 
+            colClient.HeaderText = "Cliente";
+            colClient.Name = "colClient";
+            colClient.Width = 200;
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.BorderRadius = 5;
+            guna2Button1.CustomizableEdges = customizableEdges9;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.FillColor = Color.FromArgb(64, 162, 66);
+            guna2Button1.Font = new Font("Segoe UI", 9F);
+            guna2Button1.ForeColor = Color.White;
+            guna2Button1.Location = new Point(363, 66);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Button1.Size = new Size(90, 45);
+            guna2Button1.TabIndex = 16;
+            guna2Button1.Text = "Editar";
+            // 
+            // CupomUserControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(txtPesquisa);
+            Controls.Add(guna2Button1);
+            Controls.Add(btnEditar);
+            Controls.Add(btnExcluir);
+            Controls.Add(btnAtualizar);
+            Controls.Add(lblTitulo);
+            Controls.Add(gridPedidos);
+            Name = "CupomUserControl";
+            Size = new Size(793, 482);
+            ((System.ComponentModel.ISupportInitialize)gridPedidos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox txtPesquisa;
+        private Guna.UI2.WinForms.Guna2Button btnEditar;
+        private Guna.UI2.WinForms.Guna2Button btnExcluir;
+        private Guna.UI2.WinForms.Guna2Button btnAtualizar;
+        private Label lblTitulo;
+        private DataGridView gridPedidos;
+        private DataGridViewTextBoxColumn colId;
+        private DataGridViewTextBoxColumn colDate;
+        private DataGridViewTextBoxColumn colPrice;
+        private DataGridViewTextBoxColumn colStatus;
+        private DataGridViewTextBoxColumn colClient;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
     }
 }
